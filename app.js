100e88be03872292e56a92efefb0a9e0eb68bb10
sushil26793 <82658251+sushil26793@users.noreply.github.com>
2021-04-16 18:36:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const express= require('express');
+const expressLayouts= require('express-ejs-layouts')
+const mongoose=require('mongoose')
+const falsh=require('connect-flash');
+const session=require('express-session')
+
+
+
+const app= express();
+// DB config
+const db=require('./config/keys').MongoURI;
+// Connect to mongo
+
+mongoose.connect(db,{useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>console.log('Mongo db is connected...'))
+.catch(err=>console.log(err));
+//ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
+
+// Bodyparser
+app.use(express.urlencoded({extended:false}))
+
+// Express session 
+app.use(session({
+    secret:'secret',
+    resave:true,
+    saveUninitialized:true,
+}))
+
+/// Connect Falsh
+app.use(falsh());
+
+// Global Variables 
+app.use((req,res,next)=>{
+    res.locals.success_msg=req.flash('success_msg')
+    res.locals.error_msg=req.flash('error_msg')
+    next();
+
+
+})
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Application has been started on ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MongoURI:'mongodb+srv://SushilSingh:9696300910@cluster0.gd3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+} 
